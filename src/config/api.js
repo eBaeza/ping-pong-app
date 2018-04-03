9173292211f2a92f8d5471d02128ba7429bc82b0
Edgar Baeza <holamundo@edgarbaeza.mx>
2018-04-02 22:58:04 -0500

    store for production

--- src/config/api.js
+++ src/config/api.js
@@ -1,1 +1,1 @@
-export const URL_BASE = 'http://ping-pong-api.us-west-2.elasticbeanstalk.com'
+export const URL_BASE = '//ping-pong-api.us-west-2.elasticbeanstalk.com'
--- src/store/createStore.prod.js
+++ src/store/createStore.prod.js
@@ -1,8 +1,18 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-
+import createHistory from 'history/createBrowserHistory'
+import { routerMiddleware } from  'react-router-redux'
 import rootReducer from '../reducers'
 
+export const history = createHistory()
+
 export default function configureStore (initialState) {
-  return createStore(rootReducer, initialState, applyMiddleware(thunk))
+  return createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(
+      routerMiddleware(history),
+      thunk
+    )
+  )
 };
